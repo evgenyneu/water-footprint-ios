335219172b0e7aaa2daf6c5915d2266b23486ad8
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-11-14 14:17:59 +1100

    Move table from under the keyboard

--- WaterFootprint/TableView/TableViewObject.swift
+++ WaterFootprint/TableView/TableViewObject.swift
@@ -3,11 +3,19 @@ import UIKit
 class TableViewObject: NSObject {
   @IBOutlet weak var tableView: UITableView!
   var dataSource: TableViewDataSource?
- 
+  @IBOutlet weak var bottomLayoutConstraint: NSLayoutConstraint!
+  let underKeyboardLayoutConstraint = UnderKeyboardLayoutConstraint()
+  
   func setUp() {
     tableView.tableFooterView = UIView(frame: CGRectZero)
     tableView.rowHeight = UITableViewAutomaticDimension
     tableView.estimatedRowHeight = 44
+    
+    // Get the table from under the keyboard when it appears
+    if let superview = tableView.superview {
+      underKeyboardLayoutConstraint.setup(bottomLayoutConstraint,
+        view: superview, bottomLayoutGuide: nil)
+    }
   }
   
   func showData(models: [ProductModel]) {
