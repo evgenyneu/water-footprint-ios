7444e1880f83b71fad52fb7d7ec9f8997453482c
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-11-20 14:19:24 +1100

    Russian data

---
+++ WaterFootprintTests/ViewControllers/ListViewController/Languages/RussianListViewControllerTests.swift
@@ -0,0 +1,58 @@
+import XCTest
+@testable import WaterFootprint
+
+class RussianListViewControllerTests: XCTestCase {
+  var viewController: ListViewController!
+  
+  var tableView: UITableView {
+    return viewController.tableViewObject.tableView
+  }
+  
+  override func setUp() {
+    super.setUp()
+    
+    viewController = TestHelpers.initViewController()
+    DataLanguage._currentLanguageCode = "ru"
+    viewController.beginAppearanceTransition(true, animated: false)
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    
+    viewController.endAppearanceTransition()
+    DataLanguage._currentLanguageCode = nil
+  }
+  
+  func testShowData() {
+    let numberOfRows = tableView.dataSource?.tableView(tableView, numberOfRowsInSection: 0)
+    XCTAssertEqual(231, numberOfRows)
+    
+    // Show cell
+    // ----------
+    
+    let indexPath = NSIndexPath(forRow: 145, inSection: 0)
+    
+    let tableViewCell = tableView.dataSource?.tableView(tableView, cellForRowAtIndexPath: indexPath)
+      as? TableViewCell
+    
+    XCTAssertEqual("Пшеничный крахмал", tableViewCell?.productLabel.text)
+    XCTAssertEqual("1,436", tableViewCell?.waterLitresLabel.text)
+  }
+  
+  func testSearch() {
+    viewController.searchBarObject.didSearch?("черный")
+    
+    let numberOfRows = tableView.dataSource?.tableView(tableView, numberOfRowsInSection: 0)
+    XCTAssertEqual(3, numberOfRows)
+    
+    // Show cell
+    // ----------
+    
+    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+    
+    let tableViewCell = tableView.dataSource?.tableView(tableView, cellForRowAtIndexPath: indexPath)
+      as? TableViewCell
+    
+    XCTAssertEqual("Перец свежий", tableViewCell?.productLabel.text)
+  }
+}
